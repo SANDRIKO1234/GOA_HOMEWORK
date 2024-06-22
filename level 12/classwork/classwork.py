202a4a89a4ecbf2ff6850c8545f0a8943928f0cb
SANDRIKO1234 <tamuna041982@gmail.com>
2024-06-22 20:08:35 +0400

    "second commit"

---
+++ level 12/classwork/classwork.py
@@ -0,0 +1,6 @@
+name = (input("enter your name:"))
+lastname = (input("enter your lastname:"))
+email = (input("enter your email:"))
+password = (input("enter your password:"))
+
+print("your information:", name,  lastname, email, password)
---
+++ level 12/homework/homework.py
@@ -0,0 +1,4 @@
+a= int(input("ener your dads age:"))
+b= int(input("ener your moms age:"))
+print(a + 20)
+print(b + 20)
