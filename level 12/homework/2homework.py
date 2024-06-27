2f3d0b202661b4ee023a0e64a900a275cab20065
SANDRIKO1234 <tamuna041982@gmail.com>
2024-06-27 11:27:17 -0700

    commit

---
+++ level 12/homework/2homework.py
@@ -0,0 +1,8 @@
+name = input("please enter your name:")
+lastname = input("please enter your lastname:")
+age = input("please enter your age:")
+address = input("please enter your adress:") 
+
+print(name, lastname, age, address)
+
+
---
+++ level 14/classwork/classwork2.py
@@ -0,0 +1,9 @@
+print(false and false)
+print(false and true)
+print(true and false)
+print(true and true)
+
+print(false or false)
+print(false or true)
+print(true or false)
+print(true or true)
